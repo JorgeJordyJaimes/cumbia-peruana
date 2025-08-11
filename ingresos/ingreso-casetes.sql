4cc218ad24061375ccf28f37cb09acadcdb5f1e4
Jordy Jaimes <jaimesjorgejordy@gmail.com>
2025-08-11 12:09:03 -0500

    Se añaden los archivos donde se introducirán los valores dependientes de los valores base como grupos, géneros, musicos, sellos o compositores

---
+++ ingresos/ingreso-casetes.sql
@@ -0,0 +1,31 @@
+USE Cumbia_Peruana;
+
+SELECT * FROM Albumes
+
+
+-- LOS DESTELLOS
+(1, 2, 'Llora... Llora con Los Destellos', 'HK-1077', 1988, 'Casete'), 
+
+-- GRUPO CELESTE
+(70, 1, 'Renovando Estilos', 'CTS-35', 1996, 'Casete')
+
+-- LOS DEXTER'S DE UCHIZA
+(55, 1, 'El Paso Cumbia', 'CINS-400455', 1988, 'Casete')
+
+-- LOS INVASORES DE PROGRESO
+
+(59, 1, 'La Cumbia', 'CINS-400398', 1987, 'Casete'), (59, 1, 'Aguardiente Caliente', 'CINS-400445', 1988, 'Casete'), 
+(59, 12, 'Un Recuerdo Para Uchuraccay', 'CST-120', 1989, 'Casete')
+
+-- SONIDO 2000
+(47, 1, 'Micaela', 'CINS-400449', 1988, 'Casete')
+
+-- LOS TIGRES DE TARAPOTO
+(52, 1, 'Lágrimas de Triación', 'CINS-400468', 1988, 'Casete'), (52, 1, 'El Tunchi Loco', 'CINS-400327', 1984, 'Casete'),
+
+-- CHACALÓN Y LA NUEVA CREMA
+(165, 18, 'Jugaste Conmigo', 'K-SET-038', 1990, 'Casete'), (165, 18, 'La Otra', 'Vol-5', 1991, 'Casete'), 
+(165, 18, 'Siempre Contigo', 'Vol-6', 1992, 'Casete'), (165, 18, 'No Es Fácil Perdonar', 'K-SET-172', 1991, 'Casete')
+
+-- GRUPO HALLEY
+(206, 2, 'Rumbo al Futuro', 'HK-1075', 1988, 'Casete')
---
+++ ingresos/ingreso-temas.sql
@@ -0,0 +1,4 @@
+USE cumbia_peruana;
+
+INSERT INTO Temas (id_album, id_compositor, id_genero, titulo_tema, numero_pista, duracion, lado, en_lp)
+VALUES (351, )
