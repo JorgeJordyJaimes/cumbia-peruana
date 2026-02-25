47031dd5c48bf5dc2af027540299c6e41c8c98c1
Jordy Jaimes <jaimesjorgejordy@gmail.com>
2026-02-24 20:38:56 -0500

    Se añadió un salto de página para visualizar mejor los datos de ingreso

--- datos/tipo_album.sql
+++ datos/tipo_album.sql
@@ -2,6 +2,7 @@
 USE cumbia_peruana;
 
 INSERT INTO Tipos_Album (nombre_tipo)
-VALUES  ('45'), ('LP'), ('EP'), ('Casete'), ('CD');
+VALUES  
+('45'), ('LP'), ('EP'), ('Casete'), ('CD');
 
 SELECT * FROM Tipos_Album;
