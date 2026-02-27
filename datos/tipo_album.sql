b7eeb0efcdb5ce5eaa33c6a14a006a7265091aea
Jordy Jaimes <jaimesjorgejordy@gmail.com>
2026-02-27 12:51:32 -0500

    Se borraron espacios

--- datos/tipo_album.sql
+++ datos/tipo_album.sql
@@ -3,6 +3,10 @@ USE cumbia_peruana;
 
 INSERT INTO Tipos_Album (nombre_tipo)
 VALUES  
-('45'), ('LP'), ('EP'), ('Casete'), ('CD');
+('45'),
+('LP'),
+('EP'),
+('Casete'),
+('CD');
 
 SELECT * FROM Tipos_Album;
