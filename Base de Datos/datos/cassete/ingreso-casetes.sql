f1edd10e71268cbc9682c63ed6dd2ea0570beab8
Jordy Jaimes <jaimesjorgejordy@gmail.com>
2026-09-20 19:52:15 -0500

    fix(casetes): corrige sintaxis e ids de albumes
    Añade sentencias INSERT INTO, asigna el identificador numérico de tipo
    álbum correspondiente a casete y rectifica las claves foráneas de grupos.

---
+++ Base de Datos/datos/cassete/ingreso-casetes.sql
@@ -0,0 +1,65 @@
+---------------------------------------------------------------------------------------- LOS DESTELLOS ---------------------------------------------------------------------------------
+
+-- HORÓSCOPO
+INSERT INTO albumes (id_grupo, id_sello, nombre_album, numero_catalogo, año_publicacion, id_tipo_album)
+VALUES 
+(1, 2, 'Llora... Llora con Los Destellos', 'HK-1077', 1988, 4);
+
+---------------------------------------------------------------------------------------- GRUPO CELESTE ---------------------------------------------------------------------------------
+
+-- INFOPESA
+INSERT INTO albumes (id_grupo, id_sello, nombre_album, numero_catalogo, año_publicacion, id_tipo_album)
+VALUES 
+(12, 1, 'Renovando Estilos', 'CTS-35', 1996, 4);
+
+---------------------------------------------------------------------------------- LOS DEXTER'S DE UCHIZA ----------------------------------------------------------------------------------
+
+-- INFOPESA
+INSERT INTO albumes (id_grupo, id_sello, nombre_album, numero_catalogo, año_publicacion, id_tipo_album)
+VALUES 
+(16, 1, 'El Paso Cumbia', 'CINS-400455', 1988, 4);
+
+--------------------------------------------------------------------------------- LOS INVASORES DE PROGRESO ---------------------------------------------------------------------------------
+
+-- INFOPESA
+INSERT INTO albumes (id_grupo, id_sello, nombre_album, numero_catalogo, año_publicacion, id_tipo_album)
+VALUES 
+(15, 1, 'La Cumbia', 'CINS-400398', 1987, 4), 
+(15, 1, 'Aguardiente Caliente', 'CINS-400445', 1988, 4);
+
+-- UNIVERSAL
+INSERT INTO albumes (id_grupo, id_sello, nombre_album, numero_catalogo, año_publicacion, id_tipo_album)
+VALUES 
+(15, 12, 'Un Recuerdo Para Uchuraccay', 'CST-120', 1989, 4);
+
+---------------------------------------------------------------------------------- SONIDO 2000 DE TARAPOTO ----------------------------------------------------------------------------------
+
+-- INFOPESA
+INSERT INTO albumes (id_grupo, id_sello, nombre_album, numero_catalogo, año_publicacion, id_tipo_album)
+VALUES 
+(29, 1, 'Micaela', 'CINS-400449', 1988, 4);
+
+---------------------------------------------------------------------------------- LOS TIGRES DE TARAPOTO ----------------------------------------------------------------------------------
+
+-- INFOPESA
+INSERT INTO albumes (id_grupo, id_sello, nombre_album, numero_catalogo, año_publicacion, id_tipo_album)
+VALUES 
+(21, 1, 'Lágrimas de Triación', 'CINS-400468', 1988, 4), 
+(21, 1, 'El Tunchi Loco', 'CINS-400327', 1984, 4);
+
+--------------------------------------------------------------------------------- CHACALÓN Y LA NUEVA CREMA ---------------------------------------------------------------------------------
+
+-- PRODUCCIONES MARKAHUASI
+INSERT INTO albumes (id_grupo, id_sello, nombre_album, numero_catalogo, año_publicacion, id_tipo_album)
+VALUES 
+(7, 18, 'Jugaste Conmigo', 'K-SET-038', 1990, 4), 
+(7, 18, 'La Otra', 'Vol-5', 1991, 4), 
+(7, 18, 'Siempre Contigo', 'Vol-6', 1992, 4), 
+(7, 18, 'No Es Fácil Perdonar', 'K-SET-172', 1991, 4);
+
+--------------------------------------------------------------------------------------- GRUPO HALLEY ---------------------------------------------------------------------------------------
+
+-- HORÓSCOPO
+INSERT INTO albumes (id_grupo, id_sello, nombre_album, numero_catalogo, año_publicacion, id_tipo_album)
+VALUES 
+(36, 2, 'Rumbo al Futuro', 'HK-1075', 1988, 4);
